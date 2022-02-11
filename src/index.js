0ae6ba454e0800f1f726ef3eceb178097a35179d
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2022-02-11 13:00:07 +0800

    :bookmark: update version to 1.1.0

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
 const template = document.createElement('template')
 
-const getInnerHTML = that => {
+const getInnerHTML = () => {
   return `
   <style>
   #reward-modal-container {
-    position: fixed;
-    top: 0;
-    left: 0;
     width: 100%;
-    height: 100vh;
-    display: none;
+    display: flex;
     justify-content: center;
     align-items: center;
-    background: rgba(0,0,0,0.8);
-    z-index:9999;
-    color: #130c0e;
+    z-index: 9999;
+    transform: scale(0.88);
+    will-change: transform;
+    margin: 1rem auto;
   }
   .modal-div {
-    width: 46%;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 50%;
+    max-width: 360px;
+    min-width: 300px;
     border: 1px solid #e9e9e9;
-      border-radius: 8px;
-      position: fixed;
-      box-shadow: 0 0 5px #999;
-      top:50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-    -webkit-transform: translate(-50%, -50%);
-    -moz-transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
-      background:#FFF;
-    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 0 5px #999;
+    background:#FFF;
+    padding: 10px;
     text-align: center;
+  }
+  .reward-title {
+    margin: 0;
+    font-size: 1.8rem;
   }
   @media phonemax{
     .modal-div{
@@ -60,43 +61,25 @@ const getInnerHTML = that => {
     border-radius: 1em;
     border: 1px solid #f22;
   }
-  .reward-text-div, .reward-tip-p, .reward-img-div, .reward-thx-p{
-    margin-top: 15px;
+  .reward-tip, .reward-thx{
+    margin: 5px auto;
   }
-  .reward-thx-p{
-    color: #999;
+  .reward-thx{
+    color: #999999;
   }
-  .reward-tip-p{
+  .reward-tip{
     text-align: center;
     color: #d1d1d1;
     font-size: 12px;
     line-height: 18px;
-  }
-  #open-reward-modal{
-    display: block;
-    margin: 0% auto 1% auto;
-    width: 7em;
-      height: 2.3em;
-      font-size: 1.2em;
-      line-height: 2.3;
-      color: #f2f2f2;
-    background-color: #ed1941;
-    text-decoration: none;
-    text-align: center;
-    border-radius: 2em;
-  }
-  #open-reward-modal:hover{
-    transition: all 1s;
-    color: #ffd400;
-    background-color: #f00;
   }
   .close-modal{
       position: absolute;
       right: 5%;
       top: 3%;
       background-size: 100% 100%;
-      width: 1.5em;
-      height: 1.5em;
+      width: 1.314rem;
+      height: 1.314rem;
   }
   .close-modal:before,.close-modal:after{
       position: absolute;
@@ -126,10 +109,9 @@ const getInnerHTML = that => {
   .hide{
     display: none;
   }
-
   .platform-choice {
-    width:320px;
-    margin: 25px auto 0;
+    width: 320px;
+    margin: 10px auto 0;
   }
   
   .platform-choice .platform-item {
@@ -155,15 +137,14 @@ const getInnerHTML = that => {
   .platform-choice .platform-item .platform-radio-box {
     width: 16px;
     height: 16px;
-    background: url('https://lovejade.oss-cn-shenzhen.aliyuncs.com/radio2.jpg');
+    background: url('https://image.nicelinks.site/radio2.jpeg');
     display: block;
     float: left;
-    margin-top: 5px;
-    margin-right: 14px;
+    margin-right: 15px;
   }
   
   .platform-choice .platform-item .platform-checked {
-    background: url('https://lovejade.oss-cn-shenzhen.aliyuncs.com/radio1.jpg');
+    background: url('https://image.nicelinks.site/radio1.jpeg');
   }
   
   .platform-choice .platform-logo {
@@ -176,41 +157,37 @@ const getInnerHTML = that => {
   }
 
   .platform-choice .alipay {
-    background-image: url('https://lovejade.oss-cn-shenzhen.aliyuncs.com/alipay.jpg');
+    background-image: url('https://image.nicelinks.site/alipay.jpeg');
   }
   
   .platform-choice .weichat {
-    background-image: url('https://lovejade.oss-cn-shenzhen.aliyuncs.com/wechat.jpg');
+    background-image: url('https://image.nicelinks.site/wechat.jpeg');
   }  
   </style>
  
-  <a href="javascript:;" aria-label="Open Reward" id="open-reward-modal">打赏</a>
   <div id="reward-modal-container">
-      <div class="modal-div">
-        <h1 class='reward-title-h1'>倾情打赏</h1>
-        <a href="javascript:;" aria-label="Close Modal" class="close-modal" id="close-modal"></a>
-        <p class="reward-thx-p">感谢您的支持，我将继续努力！</p>
-        <div class="reward-img-div">
-          <img class="reward-qr-img weichat active" src="//image.nicelinks.site/weixin_pay.jpg" alt="微信赞助" />
-          <img class="reward-qr-img alipay" src="//image.nicelinks.site/zhifubao_pay.jpg" alt="支付宝赞助" />
-        </div>
-
-        <div id="reward-jade-moudle" node-type="platform-choice" class="platform-choice clearfix">
-          <div class="platform-item">
-            <span node-type="check-platform" data-id="weichat" class="platform-radio-box platform-checked"></span>
-            <span class="platform-logo weichat"></span>
-          </div>
-          <div class="platform-item">
-            <span node-type="check-platform" data-id="alipay" class="platform-radio-box"></span>
-            <span class="platform-logo alipay"></span>
-          </div>
-        </div>
-
-        <div class="reward-text-div">扫码打赏，建议金额1-10元</div>
-        <p class="reward-tip-p">
-          提醒：打赏金额将直接进此方账号，无法退款，请您谨慎操作。
-        </p>
+    <div class="modal-div">
+      <h1 class='reward-title'>倾情打赏</h1>
+      <a href="javascript:;" aria-label="Close Modal" class="close-modal" id="close-modal"></a>
+      <p class="reward-thx">感谢您的支持，我将继续努力！</p>
+      <div class="reward-img-div">
+        <img class="reward-qr-img weichat active" src="//image.nicelinks.site/weixin_pay.jpg" alt="微信赞助" />
+        <img class="reward-qr-img alipay" src="//image.nicelinks.site/zhifubao_pay.jpg" alt="支付宝赞助" />
       </div>
+      <div id="reward-jade-moudle" node-type="platform-choice" class="platform-choice clearfix">
+        <div class="platform-item">
+          <span node-type="check-platform" data-id="weichat" class="platform-radio-box platform-checked"></span>
+          <span class="platform-logo weichat"></span>
+        </div>
+        <div class="platform-item">
+          <span node-type="check-platform" data-id="alipay" class="platform-radio-box"></span>
+          <span class="platform-logo alipay"></span>
+        </div>
+      </div>
+      <p class="reward-tip">
+        提醒：打赏金额将直接进此方账号，无法退款，请您谨慎操作。
+      </p>
+    </div>
   </div>
 `
 }
@@ -219,14 +196,13 @@ class RewardService extends HTMLElement {
   constructor() {
     super()
 
-    template.innerHTML = getInnerHTML(this)
+    template.innerHTML = getInnerHTML()
     this._shadowRoot = this.attachShadow({ mode: 'closed' })
     this._shadowRoot.appendChild(template.content.cloneNode(true))
     this.initEvents()
   }
 
   initEvents() {
-    this._shadowRoot.querySelector('#open-reward-modal').onclick = this.onOpenRewardClick.bind(this)
     this._shadowRoot.querySelector('#close-modal').onclick = this.onCloseClick.bind(this)
     this.radioBoxs = this._shadowRoot.querySelectorAll('.platform-radio-box')
     this.radioBoxs.forEach(item => {
@@ -234,13 +210,15 @@ class RewardService extends HTMLElement {
     })
     this.rewardModal = this._shadowRoot.querySelector('#reward-modal-container')
   }
-  onOpenRewardClick(elem) {
-    this.rewardModal.style.display = 'flex'
-  }
   onCloseClick(elem) {
     this.rewardModal.style.display = 'none'
   }
   onRadioBoxClick(event) {
+    console.log(
+      `月黑见渔灯，孤光一点萤：%c 再接再厉。`,
+      'font-weight: 600;color: #20a0ff'
+    )
+
     const currentChoiceId = event.currentTarget.dataset.id
     // deal with qrcode img
     const qrImgs = this._shadowRoot.querySelectorAll('.reward-qr-img')
